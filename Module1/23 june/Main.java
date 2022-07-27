5286628074df61593fb06d6601a0bb7fdb205585
gaurang1403 <109590977+gaurang1403@users.noreply.github.com>
2022-07-27 12:56:24 +0530

    Add files via upload

---
+++ Module1/23 june/Main.java
@@ -0,0 +1,28 @@
+class father
+{
+	public void name() 
+	{
+		System.out.println("I am Parent");
+	}
+	
+}
+class child extends father
+{
+	public void print() 
+	{
+		System.out.println("I am child");
+	}
+}
+
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		father f1 = new father();
+		child c1 = new child();
+		
+		f1.name();
+		c1.print();
+		c1.name();
+	}
+}
