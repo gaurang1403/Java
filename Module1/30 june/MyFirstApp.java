69e1837c5ee147a1a53db907bec6f30c00e83d38
gaurang1403 <109590977+gaurang1403@users.noreply.github.com>
2022-08-08 12:51:25 +0530

    Add files via upload

---
+++ Module1/30 june/MyFirstApp.java
@@ -0,0 +1,26 @@
+package june_30;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class MyFirstApp {
+
+	JFrame F;
+	JButton B;
+	public MyFirstApp() {
+		// TODO Auto-generated constructor stub
+		F = new JFrame("My First App");
+		B = new JButton("Click me");
+		
+		B.setBounds(200, 300, 100, 25);
+	
+		F.add(B);
+		F.setLayout(null);
+		F.setVisible(true);
+		F.setSize(500, 500);
+	}
+	
+	public static void main(String[] args) {
+		new MyFirstApp();
+	}
+}
