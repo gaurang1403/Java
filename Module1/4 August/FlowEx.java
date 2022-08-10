92843602f3e797a9a080c358f622daf2b6532962
gaurang1403 <109590977+gaurang1403@users.noreply.github.com>
2022-08-10 19:38:42 +0530

    Update FlowEx.java

--- Module1/4 August/FlowEx.java
+++ Module1/4 August/FlowEx.java
@@ -1,1 +1,40 @@
 
+package August_4;
+
+import java.awt.FlowLayout;
+import java.awt.LayoutManager;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class FlowEx {
+	JFrame F;
+
+	JButton b1, b2, b3, b4;
+
+	public FlowEx() {
+		// TODO Auto-generated constructor stub
+
+		F = new JFrame();
+
+		b1 = new JButton("OK");
+		b2 = new JButton("Cancel");
+		b3 = new JButton("Error");
+		b4 = new JButton("Save");
+
+		F.add(b1);
+		F.add(b2);
+		F.add(b3);
+		F.add(b4);
+
+		F.setSize(500, 500);
+		F.setLayout(new FlowLayout());
+		F.setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+		new FlowEx();
+	}
+
+}
