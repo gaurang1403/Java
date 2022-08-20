66b7479da9e48d1f500f926586c81261e15c4c50
gaurang1403 <109590977+gaurang1403@users.noreply.github.com>
2022-08-20 16:03:00 +0530

    Add files via upload

---
+++ Module1/28 July/Example_1.java
@@ -0,0 +1,30 @@
+package july_28;
+
+class A extends Thread {
+	public void run() {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Thread A: " + i);
+		}
+	}
+}
+
+class B extends Thread {
+	public void run() {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Thread B: " + i);
+		}
+	}
+}
+
+public class Example_1 {
+
+	public static void main(String[] args) {
+
+		A a = new A();
+		B b = new B();
+
+		a.start();
+		b.start();
+
+	}
+}
